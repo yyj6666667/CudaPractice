d3e0d5656fe0a8788abcdf221e3677582c377f73
benjamin <yyj6666667@gmail.com>
2026-01-14 13:12:30 +0800

    add PrefixSum_naive.cu

--- Prefix-Sum/PrefixSum_naive.cu
+++ Prefix-Sum/PrefixSum_naive.cu
@@ -3,6 +3,7 @@
  * @author Yujie-Yang (yyj6666667@gmail.com)
  * @brief a naive and ugly version with dozens of bug
           I put it here for anti-teaching example
+          排名倒数10%
  * @version 0.1
  * @date 2026-01-12
  * 
@@ -12,24 +13,22 @@
 //这是一个最优子问题
 
 __global__ void prefix_kernel(float* in,  float* out, int n) {
-    extern __shared__ float s_sum[];
-    __shared__ float  s_second_sum[1024];
-    float sum_thread = 0.0f;
+    extern __shared__ double s_sum[];
     int   size_per_thread = (n + blockDim.x - 1) / blockDim.x;
     int   in_offset_begin = threadIdx.x * size_per_thread;
     float* in_begin = in + in_offset_begin;
-    for (int i = 0; i < size_per_thread - 1; i++) {
-        //特判最后一个
-        if (threadIdx.x == blockDim.x - 1 && i >= n % size_per_thread - 1) {
-            //do nothing
-        } else {
-        in_begin[i + 1] += in_begin[i];
-        }
-        //加载进入s_mem
-        if (i == size_per_thread - 2 || (threadIdx.x == blockDim.x - 1) && i == n % size_per_thread - 1) {
-            s_sum[threadIdx.x] = in_begin[i + 1];
+    int   last_idx_per_thread = -1;
+
+    double local_sum = 0.0f;
+    for (int i = 0; i < size_per_thread; i++) {
+        int idx = i + threadIdx.x * size_per_thread;
+        if (idx < n) {
+            local_sum += in[idx];
+            in[idx]    = local_sum;
         }
     }
+
+    s_sum[threadIdx.x] = (threadIdx.x * size_per_thread < n) ? local_sum : 0.0f;
     __syncthreads();
 
     if (threadIdx.x == 0) {
@@ -39,17 +38,16 @@ __global__ void prefix_kernel(float* in,  float* out, int n) {
     }
     __syncthreads();
 
-    float* cursor = in_begin;
+    float pre_sum = (threadIdx.x > 0) ? s_sum[threadIdx.x - 1] : 0.0f;
+
+    float* cursor;
     for (int i = 0; i < size_per_thread; i++) {
-        if (threadIdx.x == blockDim.x - 1 && i == n % size_per_thread) {
-            return;
+        if (i + threadIdx.x * size_per_thread < n) {
+            cursor = in_begin + i;
+            *cursor += pre_sum;
+            out[i + threadIdx.x * size_per_thread] = *cursor;
         }
-        if (threadIdx.x != 0)
-            out[i + threadIdx.x * size_per_thread] = in[i + threadIdx.x * size_per_thread] + s_sum[threadIdx.x - 1];
-                                                    //直接命名为变量吧， 太丑了
     }
-
-
 }
 
 // input, output are device pointers
@@ -57,7 +55,7 @@ extern "C" void solve(const float* input, float* output, int N) {
     //solution: div into x grids, each 做梯度加， 最后统一聚合， 每个线程负责一个小批次
     //其实可以在grid 维度上在做文章， 先写个简陋的
     int n_threads = 256;
-    int s_size    = n_threads * sizeof(float);
+    int s_size    = n_threads * sizeof(double);
     float* in_mut = NULL;
     cudaMalloc(&in_mut, N * sizeof(float));
     cudaMemcpy(in_mut, input, N * sizeof(float), cudaMemcpyDeviceToDevice);
